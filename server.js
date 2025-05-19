6d672c3eae7f425baa19158a309436e1f1a8f270
Imran Ahmad <ahmadimrankh99@gmail.com>
2025-05-19 11:21:37 +0500

    Committed

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const FormSchema = new mongoose.Schema({
 const FormModel = mongoose.model("Form", FormSchema);
 
 // Route to receive form data
-app.post("https://bisep.up.railway.app/submit", async (req, res) => {
+app.post("/submit", async (req, res) => {
   const formData = new FormModel(req.body);
   await formData.save();
   res.send("Data saved to MongoDB!");
