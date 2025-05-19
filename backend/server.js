33ac40a4fad84c980fddb5d2b9d39fc7157b2679
Imran Ahmad <ahmadimrankh99@gmail.com>
2025-05-19 12:43:03 +0500

    Committed

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Replace this with your own MongoDB connection string
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected"))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
   // app.use(express.static('public'));
