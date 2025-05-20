81a5910797ba4267a27666fd560cff18fb953e67
Imran Ahmad <ahmadimrankh99@gmail.com>
2025-05-20 08:40:44 +0500

    Committed

--- backend/server.js
+++ backend/server.js
@@ -41,10 +41,14 @@ app.post("/submit", async (req, res) => {
   res.send("Data saved to MongoDB!");
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+// app.listen(3000, () => console.log("Server running on http://localhost:3000"));
 
 
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 
 
