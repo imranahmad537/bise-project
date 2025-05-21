44922346ba7cc0f11040fe4864b7b8cb49479333
Imran Ahmad <ahmadimrankh99@gmail.com>
2025-05-21 10:26:32 +0500

    Commit

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.post("/submit", async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
